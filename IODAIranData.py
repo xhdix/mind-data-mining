9a936e43d2a0b53c2704a93737b882ea1431492a
xhdix <xhdix@yahoo.com>
2021-10-30 19:54:52 +0300

    add sleep for one sec in the fetch loop

--- IODAIranData.py
+++ IODAIranData.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
+# source: https://gist.github.com/hellais/de19a104681402e9b9b63df73dd0f5d7
 
 # In[4]:
 
 
-import sys
-import requests
-import json
-import pandas as pd
 import csv
-from datetime import timezone, datetime
+import json
+import sys
+from datetime import datetime, timezone
+from time import sleep
 
+import pandas as pd
+import requests
 
 # In[5]:
 
@@ -27,75 +29,76 @@ untilstr = datetime.utcfromtimestamp(untilts).strftime('%Y-%m-%dT%H-%M-%SZ')
 
 def count_expression(asn):
     return {
-  "type": "function",
-  "func": "removeEmpty",
-  "args": [
-    {
-      "type": "function",
-      "func": "group",
-      "args": [
-        {
-          "type": "function",
-          "func": "alias",
-          "args": [
+        "type": "function",
+        "func": "removeEmpty",
+        "args": [
             {
-              "type": "path",
-              "path": "bgp.prefix-visibility.asn.{}.v4.visibility_threshold.min_50%_ff_peer_asns.visible_slash24_cnt".format(asn)
-            },
-            {
-              "type": "constant",
-              "value": "BGP (# Visible \/24s)"
-            }
-          ]
-        },
-        {
-          "type": "function",
-          "func": "alias",
-          "args": [
-            {
-              "type": "path",
-              "path": "darknet.ucsd-nt.non-erratic.routing.asn.{}.uniq_src_ip".format(asn)
-            },
-            {
-              "type": "constant",
-              "value": "Darknet (# Unique Source IPs)"
-            }
-          ]
-        },
-        {
-          "type": "function",
-          "func": "alias",
-          "args": [
-            {
-              "type": "function",
-              "func": "sumSeries",
-              "args": [
-                {
-                  "type": "function",
-                  "func": "keepLastValue",
-                  "args": [
+                "type": "function",
+                "func": "group",
+                "args": [
                     {
-                      "type": "path",
-                      "path": "active.ping-slash24.asn.{}.probers.team-1.caida-sdsc.*.up_slash24_cnt".format(asn)
+                        "type": "function",
+                        "func": "alias",
+                        "args": [
+                            {
+                                "type": "path",
+                                "path": "bgp.prefix-visibility.asn.{}.v4.visibility_threshold.min_50%_ff_peer_asns.visible_slash24_cnt".format(asn)
+                            },
+                            {
+                                "type": "constant",
+                                "value": "BGP (# Visible \/24s)"
+                            }
+                        ]
                     },
                     {
-                      "type": "constant",
-                      "value": 1
+                        "type": "function",
+                        "func": "alias",
+                        "args": [
+                            {
+                                "type": "path",
+                                "path": "darknet.ucsd-nt.non-erratic.routing.asn.{}.uniq_src_ip".format(asn)
+                            },
+                            {
+                                "type": "constant",
+                                "value": "Darknet (# Unique Source IPs)"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "function",
+                        "func": "alias",
+                        "args": [
+                            {
+                                "type": "function",
+                                "func": "sumSeries",
+                                "args": [
+                                    {
+                                        "type": "function",
+                                        "func": "keepLastValue",
+                                        "args": [
+                                            {
+                                                "type": "path",
+                                                "path": "active.ping-slash24.asn.{}.probers.team-1.caida-sdsc.*.up_slash24_cnt".format(asn)
+                                            },
+                                            {
+                                                "type": "constant",
+                                                "value": 1
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                            {
+                                "type": "constant",
+                                "value": "Active Probing (# \/24s Up)"
+                            }
+                        ]
                     }
-                  ]
-                }
-              ]
-            },
-            {
-              "type": "constant",
-              "value": "Active Probing (# \/24s Up)"
+                ]
             }
-          ]
-        }
-      ]
+        ]
     }
-  ]
-}
+
 
 def get_chart(asn='12880', since='1578960000', until='1579046400', expression=count_expression):
     base_url = 'https://ioda.caida.org/data/ts/json'
@@ -123,44 +126,44 @@ def get_chart(asn='12880', since='1578960000', until='1579046400', expression=co
 
 
 asn_list = [
-    ['44244', 'IranCell','Filternet'],
-    ['197207', 'MCCI','Filternet'],
-    ['58224', 'IranTelecomCo','Filternet'],
-    ['12880', 'ITC','Gateway'],
-    ['49666', 'TIC','Gateway'],
-    ['48159', 'TIC','Gateway'],
-    ['6736', 'IRANET-IPM','Gateway'],
-    ['29049', 'DeltaTelecom','Transit'],
-    ['6453', 'TATA','Transit'],
-    ['5511', 'Orange','Transit'],
-    ['6939', 'HurricaneElectric','Transit'],
-    ['6762', 'TelecomItaliaSpa','Transit'],
-    ['1299', 'TeliaCoAB','Transit'],
-    ['39533', 'AsymptoNetKft','Transit'],
-    ['200612', 'GulfBridgeInt','Transit'],
-    ['49832', 'QBIC-fka-Rikeza','Transit'],
-    ['1239', 'Sprint','Transit'],
-    ['31549', 'Shatel','Filternet'],
-    ['16322', 'ParsOnline','Filternet'],
-    ['41689', 'Asiatech','Filternet'],
-    ['43754', 'Asiatech','Filternet'],
-    ['57218', 'Rightel','Filternet'],
-    ['62140', 'Rightel','Filternet'],
-    ['209459', 'AbrArvan','Filternet'],
-    ['202468', 'AbrArvan','Filternet'],
-    ['50810', 'Mobinnet','Filternet'],
-    ['39501', 'NGSAS-aka-Sabanet','Filternet'],
-    ['48147', 'AminIDC','Filternet'],
-    ['49100', 'IR-THR-PTE','Filternet'],
-    ['47262', 'Hamara','Filternet'],
-    ['56402', 'Dadehgostar','Filternet'],
-    ['62442', 'DadeSamaneFanava','Filternet'],
-    ['39164', 'DadeSamaneFanava','Filternet'],
-    ['61036', 'FanavaGroup','Filternet'],
-    ['60637', 'FanavaGroup','Filternet'],
-    ['41881', 'FanavaGroup','Filternet'],
-    ['42337', 'Respina','Filternet'],
-    ['25124', 'Datak','Filternet']
+    ['44244', 'IranCell', 'Filternet'],
+    ['197207', 'MCCI', 'Filternet'],
+    ['58224', 'IranTelecomCo', 'Filternet'],
+    ['12880', 'ITC', 'Gateway'],
+    ['49666', 'TIC', 'Gateway'],
+    ['48159', 'TIC', 'Gateway'],
+    ['6736', 'IRANET-IPM', 'Gateway'],
+    ['29049', 'DeltaTelecom', 'Transit'],
+    ['6453', 'TATA', 'Transit'],
+    ['5511', 'Orange', 'Transit'],
+    ['6939', 'HurricaneElectric', 'Transit'],
+    ['6762', 'TelecomItaliaSpa', 'Transit'],
+    ['1299', 'TeliaCoAB', 'Transit'],
+    ['39533', 'AsymptoNetKft', 'Transit'],
+    ['200612', 'GulfBridgeInt', 'Transit'],
+    ['49832', 'QBIC-fka-Rikeza', 'Transit'],
+    ['1239', 'Sprint', 'Transit'],
+    ['31549', 'Shatel', 'Filternet'],
+    ['16322', 'ParsOnline', 'Filternet'],
+    ['41689', 'Asiatech', 'Filternet'],
+    ['43754', 'Asiatech', 'Filternet'],
+    ['57218', 'Rightel', 'Filternet'],
+    ['62140', 'Rightel', 'Filternet'],
+    ['209459', 'AbrArvan', 'Filternet'],
+    ['202468', 'AbrArvan', 'Filternet'],
+    ['50810', 'Mobinnet', 'Filternet'],
+    ['39501', 'NGSAS-aka-Sabanet', 'Filternet'],
+    ['48147', 'AminIDC', 'Filternet'],
+    ['49100', 'IR-THR-PTE', 'Filternet'],
+    ['47262', 'Hamara', 'Filternet'],
+    ['56402', 'Dadehgostar', 'Filternet'],
+    ['62442', 'DadeSamaneFanava', 'Filternet'],
+    ['39164', 'DadeSamaneFanava', 'Filternet'],
+    ['61036', 'FanavaGroup', 'Filternet'],
+    ['60637', 'FanavaGroup', 'Filternet'],
+    ['41881', 'FanavaGroup', 'Filternet'],
+    ['42337', 'Respina', 'Filternet'],
+    ['25124', 'Datak', 'Filternet']
 ]
 
 
@@ -168,7 +171,8 @@ asn_list = [
 
 
 with open(sincestr + '_' + untilstr + '_ioda-iran.csv', 'w+') as out_file:
-    writer = csv.DictWriter(out_file, fieldnames=['datasource', 'value', 'as_name', 'datetime', 'timestamp', 'net_type'])
+    writer = csv.DictWriter(out_file, fieldnames=[
+                            'datasource', 'value', 'as_name', 'datetime', 'timestamp', 'net_type'])
     writer.writerow({
         'value': 'Value',
         'as_name': 'Network',
@@ -185,7 +189,8 @@ with open(sincestr + '_' + untilstr + '_ioda-iran.csv', 'w+') as out_file:
         until = untilts
         print('Downloading {}'.format(asnum))
         try:
-            chart = get_chart(asn=asnum, since='{}'.format(since), until='{}'.format(until))
+            chart = get_chart(asn=asnum, since='{}'.format(
+                since), until='{}'.format(until))
         except Exception as exc:
             if format(exc) == "'NoneType' object has no attribute 'keys'":
                 print('there is no data for ' + asnum)
@@ -212,10 +217,6 @@ with open(sincestr + '_' + untilstr + '_ioda-iran.csv', 'w+') as out_file:
                 })
                 idx += 1
                 timestamp += step
-
+        sleep(1)
 
 # In[ ]:
-
-
-
-
